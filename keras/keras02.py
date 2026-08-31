6dcecddddb0cb389b214e3cd8223bc5c53e72bef
osangmin <osm2345679@naver.com>
2026-08-31 17:32:24 +0900

    Day1_keras

--- keras/keras02.py
+++ keras/keras02.py
@@ -1,13 +1,10 @@
-import tensorflow as tf
-print(tf.__version__)
-
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 import numpy as np
 
 #1. 데이터
-x = np.array([1,2,3])
-y = np.array([1,2,3])
+x = np.array([1,2,3,4,5,6])
+y = np.array([1,2,3,4,5,6])
 
 #2. 모델구성
 model = Sequential()
@@ -15,8 +12,10 @@ model.add(Dense(1, input_dim=1))
 
 #3. 컴파일, 훈련
 model.compile(loss='mse', optimizer='adam')
-model.fit(x, y, epochs=200)
+model.fit(x, y, epochs=300)
 
 #4. 평가, 예측
-result = model.predict(np.array([4]))
-print("4의 예측값 : ", result)
+loss = model.evaluate(x, y)
+print("loss : ", loss)
+result = model.predict(np.array([1,2,3,4,5,6,7]))
+print("7의 예측값 : ", result)
