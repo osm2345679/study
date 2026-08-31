3cf1ee403572474022aab1f1953a661c58a8d9f3
osangmin <osm2345679@naver.com>
2026-08-31 15:00:14 +0900

    Initial commit

---
+++ keras/keras01.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+print(tf.__version__)
+
+from tensorflow.keras.models import Sequntial
+from tensorflow.keras.layers import Dense
+import numpy as np
+
+#1. 데이터
+x = np.array([1,2,3])
+y = np.array([1,2,3])
+
+#2. 모델구성
